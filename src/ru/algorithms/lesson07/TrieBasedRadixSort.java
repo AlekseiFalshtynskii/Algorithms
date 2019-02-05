596219efe2a344f8e117e26ce4b983dfcc205bb8
AlekseiFalshtynskii <aleksei.falshtynskii@gmail.com>
2019-02-05 21:58:58 +0300

    lesson07

--- src/ru/algorithms/lesson07/TrieBasedRadixSort.java
+++ src/ru/algorithms/lesson07/TrieBasedRadixSort.java
@@ -6,38 +6,43 @@ import java.util.List;
 
 import static java.lang.Math.log10;
 import static java.lang.Math.pow;
-import static java.lang.System.arraycopy;
 import static java.util.Arrays.stream;
 import static java.util.concurrent.ThreadLocalRandom.current;
 
 public class TrieBasedRadixSort {
-    private static final int SIZE = 100;
+    private static final int SIZE = 10;
 
     public static void main(String[] args) {
         int[] arr = current().ints(SIZE, 0, 1000).toArray();
         System.out.println(Arrays.toString(arr));
-        trieBasedSort(arr);
+        arr = trieBasedSort(arr);
         System.out.println(Arrays.toString(arr));
+        System.out.println();
     }
 
-    static void trieBasedSort(int[] arr) {
+    static int[] trieBasedSort(int[] arr) {
         Trie trie = new Trie(arr);
-        trie.traverse(arr);
+        int index = current().nextInt(0, arr.length - 1);
+        System.out.println("Удален элемент " + arr[index]);
+        trie.remove(arr[index]);
+        return trie.traverse(arr);
     }
 
     private static class Trie {
         private TrieNode root;
         private int depth;
+        private int size;
         private int sorted_i;
 
         Trie(int[] arr) {
+            size = arr.length;
             depth = (int) log10(stream(arr).max().orElse(0));
             for (int i = 0; i < arr.length; i++) {
                 insert(arr[i], i);
             }
         }
 
-        private void insert(int n, int index) {
+        void insert(int n, int index) {
             if (root == null) {
                 root = new TrieNode();
             }
@@ -45,7 +50,7 @@ public class TrieBasedRadixSort {
             int exp = (int) pow(10, depth);
             for (int i = 0; i <= depth; i++) {
                 int digit = n / exp;
-                if (currentNode.children == null || currentNode.getChild(digit) == null) {
+                if (currentNode.getChild(digit) == null) {
                     currentNode.addChild(digit);
                 }
                 currentNode = currentNode.getChild(digit);
@@ -55,11 +60,31 @@ public class TrieBasedRadixSort {
             currentNode.addIndex(index);
         }
 
-        void traverse(int[] arr) {
-            int[] sorted = new int[arr.length];
+        void remove(int n) {
+            int exp = (int) pow(10, depth);
+            remove(n, root, exp);
+            size--;
+        }
+
+        private void remove(int n, TrieNode node, int exp) {
+            if (node == null || node.getChildrenCount() == 0) {
+                return;
+            }
+            remove(n % exp, node.getChild(n / exp), exp / 10);
+
+            if (exp == 1 || (node.getChild(n / exp) != null && node.getChild(n / exp).isDeleted())) {
+                node.removeChild(n / exp);
+                if (node.getChildrenCount() == 0) {
+                    node.setDeleted();
+                }
+            }
+        }
+
+        int[] traverse(int[] arr) {
+            int[] sorted = new int[size];
             sorted_i = 0;
             traverse(root, arr, sorted);
-            arraycopy(sorted, 0, arr, 0, arr.length);
+            return sorted;
         }
 
         private void traverse(TrieNode node, int[] arr, int[] sorted) {
@@ -77,8 +102,10 @@ public class TrieBasedRadixSort {
         }
 
         private static class TrieNode {
+            private int childrenCount;
             private TrieNode[] children;
             private List<Integer> indexes;
+            private boolean deleted;
 
             TrieNode() {
                 children = new TrieNode[10];
@@ -89,9 +116,27 @@ public class TrieBasedRadixSort {
                 return children[index];
             }
 
+            void setDeleted() {
+                deleted = true;
+            }
+
+            boolean isDeleted() {
+                return deleted;
+            }
+
+            void removeChild(int index) {
+                children[index] = null;
+                childrenCount--;
+            }
+
             void addChild(int index) {
                 TrieNode node = new TrieNode();
                 children[index] = node;
+                childrenCount++;
+            }
+
+            int getChildrenCount() {
+                return childrenCount;
             }
 
             List<Integer> getIndexes() {
